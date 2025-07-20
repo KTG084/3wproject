f01e40f528de682e242e20ca7ffb63b9e78fc356
KTG084 <karanjyotimedhi5678@gmail.com>
2025-07-21 00:10:10 +0530

    new2

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,8 +1,7 @@
 import { prisma } from "@/lib/prisma";
 
-
 import ClientLeaderboard from "@/components/ClientLeaderboard";
-
+export const dynamic = "force-dynamic";
 export default async function Leaderboard() {
   const users = await prisma.user.findMany({
     orderBy: { totalPoints: "desc" },
@@ -14,7 +13,7 @@ export default async function Leaderboard() {
   });
 
   console.log(users);
-  console.log("its working")
+  console.log("its working");
 
   return <ClientLeaderboard users={users} />;
 }
